919ac81982d768eea6b0f13cfd7bc8ce7d643f64
Nefelijm <sistemasnik20@gmail.com>
2018-04-08 12:04:11 -0500

    funcion buscar probando

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -2,8 +2,10 @@ function searchForm(){
  
     $(document).ready(() =>{
       //llamamos al boton y al buscador
+      const search_form = document.getElementById('search-form');
       const search_form_field = document.getElementById("search-field");
       const search_form_button = document.getElementById("search-button");
+      const results_content = document.getElementById("content");
       const search_text = $(search_form_field);
       
       //Validando que el boton se habilite si es mas de dos caracteres
@@ -14,19 +16,27 @@ function searchForm(){
       //autocompletado
       const URL = '/books-schema.json';
 
-      loadJSON(URL,(response) =>{ 
+      loadJSON(URL,(response) =>{
+        //condicion 
         const awesompleteOptions = {
           minChars: 3,
           maxItems: 7,
         };
         
+        //recorriendo la data
         const list = response.data.map((item) => item.title);
-
+          
           awesomplete = new Awesomplete(search_form_field, awesompleteOptions);
           awesomplete.list = list;
         
         })
     
+      //buscador
+      $(search_form).on('submit', (event) => {
+        event.preventDefault();
+         alert('f');
+      });
+  
   
      
      
