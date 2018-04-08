8fd14127e8271f833297026d8523317ea7ae5831
Nefelijm <sistemasnik20@gmail.com>
2018-04-08 11:46:08 -0500

    Funcion para el autocompletado

--- public/scripts/search-form.js
+++ public/scripts/search-form.js
@@ -5,12 +5,32 @@ function searchForm(){
       const search_form_field = document.getElementById("search-field");
       const search_form_button = document.getElementById("search-button");
       const search_text = $(search_form_field);
-
+      
+      //Validando que el boton se habilite si es mas de dos caracteres
       search_text.on('keyup', () => {
         $(search_form_button).prop('disabled', !(search_text.val().length > 2));
       });
 
-      
+      //autocompletado
+      const URL = '/books-schema.json';
+
+      loadJSON(URL,(response) =>{ 
+        const awesompleteOptions = {
+          minChars: 3,
+          maxItems: 7,
+        };
+        
+        const list = response.data.map((item) => item.title);
+
+          awesomplete = new Awesomplete(search_form_field, awesompleteOptions);
+          awesomplete.list = list;
+        
+        })
+    
+  
+     
+     
+
     });
       
 }
