e2e3835d1f5872b93c7d544e001cd5f4eff660e0
Nefelijm <sistemasnik20@gmail.com>
2018-04-08 10:16:53 -0500

    primer requerimiento habilitando boton

--- public/scripts/search-form.js
+++ public/scripts/search-form.js
@@ -1,3 +1,16 @@
 function searchForm(){
-    // code here
+ 
+    $(document).ready(() =>{
+      //llamamos al boton y al buscador
+      const search_form_field = document.getElementById("search-field");
+      const search_form_button = document.getElementById("search-button");
+      const search_text = $(search_form_field);
+
+      search_text.on('keyup', () => {
+        $(search_form_button).prop('disabled', !(search_text.val().length > 2));
+      });
+
+      
+    });
+      
 }
